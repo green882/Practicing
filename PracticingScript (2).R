c1ab1436074c5e1f37e32cd39d2332141f0ee127
Jane Doe <RebeccaB@ndph.ox.ac.uk>
2026-03-10 14:04:55 +0000

    Initial commit

---
+++ PracticingScript (2).R
@@ -0,0 +1,19 @@
+
+library(here)
+
+# Tells R how to get to the file, from the working directory
+data <- read.csv(here("Data", "Raw", "ShipAccidents.csv"))
+
+# install renv
+install.packages("renv")
+
+# 1. Initialize renv
+renv::init()
+
+# 2. Install packages
+install.packages("tidyverse")
+install.packages("here")
+
+# 3. SNAPSHOT to record versions
+renv::snapshot()
+y
